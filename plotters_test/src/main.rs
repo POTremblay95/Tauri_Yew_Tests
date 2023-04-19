a8582bbb2cdae3f0d70ef80ffed172205d95d751
POTremblay95 <46535797+POTremblay95@users.noreply.github.com>
2023-04-19 17:20:40 -0400

    change plot on click

--- plotters_test/src/main.rs
+++ plotters_test/src/main.rs
@@ -14,23 +14,35 @@ pub struct PlotProps {}
 */
 pub struct Plot {
     canvas: NodeRef,
+    draw_x2: bool,
+}
+
+impl Plot {
+    fn get_label(&self) -> Option<String> {
+        if self.draw_x2 {
+            Some(String::from("y = x^2"))
+        } else {
+            Some(String::from("y = sin(x)"))
+        }
+    }
 }
 
 impl Component for Plot {
     type Message = PlotMsg;
     type Properties = ();
 
-    fn view(&self, _ctx: &Context<Self>) -> Html {
+    fn view(&self, ctx: &Context<Self>) -> Html {
         html! (
-          <div>
-            <canvas ref = {self.canvas.clone()}/>
+          <div style="width: 100%; height: 100%;">
+            <canvas style="width: 100%; height: 100%;" onclick={ctx.link().callback(|_| PlotMsg::Redraw)} ref = {self.canvas.clone()}/>
           </div>
         )
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            PlotMsg::Redraw => {
+            PlotMsg::Nothing => true,
+            _ => {
                 let element: HtmlCanvasElement = self.canvas.cast().unwrap();
 
                 let rect = element.get_bounding_client_rect();
@@ -40,30 +52,44 @@ impl Component for Plot {
                 let backend = CanvasBackend::with_canvas_object(element).unwrap();
 
                 let drawing_area = backend.into_drawing_area();
-                drawing_area.fill(&RGBColor(200, 200, 200)).unwrap();
+                drawing_area.fill(&RGBColor(200, 200, 200)).unwrap(); // Gray background
+
+                let plot_label = self.get_label().expect("There was an error.");
 
                 let mut chart = ChartBuilder::on(&drawing_area)
-                    .caption("y=x^2", ("sans-serif", 14).into_font())
-                    .margin(5)
+                    .caption(&plot_label, ("sans-serif", 14).into_font())
+                    .margin(10)
                     .x_label_area_size(30)
                     .y_label_area_size(30)
-                    .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32)
+                    .build_cartesian_2d(-1f32..1f32, -1f32..1f32) //x axis range and y axis range
                     .unwrap();
 
                 chart.configure_mesh().draw().unwrap();
 
-                chart
-                    .draw_series(LineSeries::new(
-                        (-50..=50).map(|x| x as f32 / 50.0).map(|x| (x, x * x)),
-                        &RED,
-                    ))
-                    .unwrap()
-                    .label("y = x^2")
-                    .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+                if self.draw_x2 {
+                    chart
+                        .draw_series(LineSeries::new(
+                            (-50..=50).map(|x| x as f32 / 50.0).map(|x| (x, x * x)),
+                            &RED,
+                        ))
+                        .unwrap()
+                        .label(&plot_label)
+                        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+                } else {
+                    chart
+                        .draw_series(LineSeries::new(
+                            (-50..=50).map(|x| x as f32 / 50.0).map(|x| (x, x.sin())),
+                            &RED,
+                        ))
+                        .unwrap()
+                        .label(&plot_label)
+                        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+                }
+                // Change the member for next redraw
+                self.draw_x2 = !self.draw_x2;
 
                 false
             }
-            _ => true,
         }
     }
 
@@ -71,6 +97,7 @@ impl Component for Plot {
         ctx.link().send_message(PlotMsg::Redraw);
         Plot {
             canvas: NodeRef::default(),
+            draw_x2: true,
         }
     }
 }
